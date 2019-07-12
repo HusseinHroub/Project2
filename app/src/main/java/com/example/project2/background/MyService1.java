9e32a0754ce61060605bf8c3f25145cf29ca7c07
DayLightHroub <husseinhroub311@gmail.com>
2019-07-12 11:39:04 +0300

    intital commit

---
+++ app/src/main/java/com/example/project2/background/MyService1.java
@@ -0,0 +1,161 @@
+package com.example.project2.background;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.project2.R;
+import com.example.project2.common.NotificationManagerr;
+
+public class MyService1 extends Service {
+    private boolean running = true;
+    private int i = 0;
+    private class DownloadFilesTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            while (running) {
+                try {
+                    Thread.sleep(5000);
+                    showNotificaiton(++i);
+                    System.out.println(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+            return null;
+        }
+
+
+        private void showNotificaiton(int i) {
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MyService1.this);
+            notificationManager.notify(3, NotificationManagerr.getNotificationBuilder(MyService1.this,
+                    CHANNEL_ID,
+                    "Thread notfication",
+                    "i is: " + i,
+                    true).build());
+        }
+
+        public void stopRunning() {
+            running = false;
+        }
+    }
+
+    private static final String CHANNEL_ID = "servicechannel";
+    private final int FOR_GROUND_ID = 1;
+    private ImportantThread importantThread;
+
+
+    // Handler that receives messages from the thread
+
+
+    @Override
+    public void onCreate() {
+
+
+        setInForGround();
+        System.out.println("onCreate()");
+        new DownloadFilesTask().execute();
+
+
+    }
+
+    private void setInForGround() {
+        NotificationManagerr.createNotificaitonChannel("Listening notifcation",
+                "if this notifcaiton is on, this means that a listening background service is working",
+                CHANNEL_ID,
+                getApplicationContext());
+        NotificationCompat.Builder notificationBuilder = NotificationManagerr.getNotificationBuilder(getApplicationContext(),
+                CHANNEL_ID,
+                "Listenintg",
+                "ok I am listening",
+                true);
+
+
+        //intent to handle the close button
+        Intent endService = new Intent(getApplicationContext(), BoradCastRec.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),
+                1,
+                endService,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        notificationBuilder.addAction(R.drawable.wave_icon, "Cancle test", pendingIntent);
+        startForeground(FOR_GROUND_ID, notificationBuilder.build());
+
+
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return START_STICKY;
+
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // We don't provide binding, so return null
+        return null;
+    }
+
+
+    @Override
+    public void onDestroy() {
+
+        Toast.makeText(this, "service stopped", Toast.LENGTH_SHORT).show();
+        running = false;
+//        importantThread.stopRunning();
+
+//        stopSelf();
+    }
+
+
+    //V@1
+//    private void setInForGround() {
+//        Intent notificationIntent = new Intent(this, MainActivity.class);
+//        PendingIntent pendingIntent =
+//                PendingIntent.getActivity(this, 0, notificationIntent, 0);
+//
+//
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//            String Channel_Id = createNotificationChannel("gps_updater", "gps_service");
+//            Notification notification =
+//                    new Notification.Builder(this, Channel_Id)
+//                            .setContentTitle("Gps App")
+//                            .setContentText("Updating Gps")
+//                            .setSmallIcon(android.R.drawable.sym_def_app_icon)
+//                            .setContentIntent(pendingIntent)
+////                            .setTicker("test")
+//                            .build();
+//
+//            startForeground(FOR_GROUND_ID, notification);
+//        }
+//
+//
+//    }
+
+//    @RequiresApi(api = Build.VERSION_CODES.O)
+//    private String createNotificationChannel(String channelId, String channelName) {
+//        NotificationChannel channel = new NotificationChannel(channelId,
+//                channelName, NotificationManager.IMPORTANCE_HIGH);
+//
+////        channel.setLightColor(Color.BLUE);
+////        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+//        NotificationManager service = getSystemService(NotificationManager.class);
+//        service.createNotificationChannel(channel);
+//
+//        return channelId;
+//    }
+
+}
